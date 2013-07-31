ed467dbfd73b35e28a88c27d4a938ad3dc2368cc
Travis McHattie <travis.mchattie@thinksolid.com>
2013-07-30 17:37:24 -0700

    Updated testing

--- test/test-solid-logger.js
+++ test/test-solid-logger.js
@@ -1,25 +1,19 @@
 var logger = require('../lib/solid-logger'),
-    fileAdapter = require('../lib/adapters/file.js'),
     testDirectory = __dirname,
     libDirectory  = testDirectory.substr(0, testDirectory.indexOf('/test'));
 
 logger.init({
-    namespace: "test",
     adapters: [{
         type: "file",
-        path: libDirectory + "../log/test.log"
+        path: libDirectory + "/log/test.log",
+        application: 'grasshopper-api',
+        machine: 'dev-server'
+    },{
+            type: "file",
+            path: libDirectory + "/log/backup-test.log",
+            application: 'grasshopper-api',
+            machine: 'dev-server'
     }]
 });
 
-fileAdapter.init({
-    type: "file",
-    path: libDirectory + "/log/test.log",
-    application: 'grasshopper-api',
-    machine: 'dev-server'
-});
-
-fileAdapter.write('error', 'login::methodname', 'This is my exception', function(result){
-    console.log('finished result');
-    console.log(result);
-
-});
+logger.debug('MYSCOPE', 'This is my debugging message.');
