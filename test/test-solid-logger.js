e61068c9f99c96b82763ca2232b89d33874a436c
Travis McHattie <travis.mchattie@thinksolid.com>
2013-10-29 07:56:57 -0700

    Updated tests.

--- test/test-solid-logger.js
+++ test/test-solid-logger.js
@@ -1,19 +1,32 @@
-var logger = require('../lib/solid-logger'),
-    testDirectory = __dirname,
-    libDirectory  = testDirectory.substr(0, testDirectory.indexOf('/test'));
+var should = require('chai').should();
 
-logger.init({
-    adapters: [{
-        type: "file",
-        path: libDirectory + "/log/test.log",
-        application: 'grasshopper-api',
-        machine: 'dev-server'
-    },{
-            type: "file",
-            path: libDirectory + "/log/backup-test.log",
-            application: 'grasshopper-api',
-            machine: 'dev-server'
-    }]
+describe('Testing Solid Logger', function(){
+    var path = require('path');
+
+    describe("Test Logger using a file adapter and the config object passed in through the init function.", function() {
+        it('should return a valid logger object', function() {
+            var logger = require('../lib/solid-logger').init({
+                adapters: [{
+                    type: "file",
+                    path: path.resolve("./log/test.log"),
+                    application: 'grasshopper-api',
+                    machine: 'dev-server'
+                },{
+                    type: "file",
+                    path: path.resolve("./log/backup-test.log"),
+                    application: 'grasshopper-api',
+                    machine: 'dev-server'
+                }]
+            });
+
+            logger.should.be.a("object");
+        });
+
+        it('should return a valid logger object when configuring with a file.', function() {
+            var logger = require('../lib/solid-logger').initWithFile("./config/config.example.json");
+
+            logger.should.be.a("object");
+            logger.debug("FILE_INIT_EXAMPLE", "DOES THIS SHOW UP?");
+        });
+    });
 });
-
-logger.debug('MYSCOPE', 'This is my debugging message.');
