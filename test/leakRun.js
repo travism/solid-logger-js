ec1aee36da3613af5982c13accf64814fca83387
Peter Ajtai <peter.ajtai@thinksolid.com>
2015-08-05 10:55:51 -0700

    Fixing memory leak - closes #10

---
+++ test/leakRun.js
@@ -0,0 +1,19 @@
+var logger = require('../lib/solid-logger');
+
+var start = Date.now();
+
+logger.init({
+    adapters: [{
+        type: 'console',
+        application: 'grasshopper-api',
+        machine: 'dev-server'
+    }]
+});
+
+setInterval(reportMemory, 0);
+
+function reportMemory() {
+    var heap = process.memoryUsage().heapUsed / (1024 * 1024);
+    var memPerSecond = heap / (Date.now() - start) * 1000;
+    logger.debug(heap.toFixed(2) + 'mb (' + memPerSecond.toFixed(2) + 'mb/s) \r');
+}
