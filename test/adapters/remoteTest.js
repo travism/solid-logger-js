063f39788526babf1ba6d9dbcb1624bde6c2850e
Peter Ajtai <peter.ajtai@thinksolid.com>
2015-12-23 08:20:33 -0800

    Remote tests are passing alone but not with npm test

--- test/adapters/remoteTest.js
+++ test/adapters/remoteTest.js
@@ -1,36 +1,102 @@
 'use strict';
 
 var Logger = require('../../lib/solid-logger'),
+    BB = require('bluebird'),
     util = require('./util'),
     express = require('express'),
-    logger = null;
+    logger = Logger.init({
+        adapters: [{
+            type: 'remote',
+            verb: 'GET',
+            url: 'http://localhost:4321/log',
+            application: 'test',
+            machine: 'test-host'
+        }]
+    }),
+    should = require('chai').should(),
+    app,
+    server,
+    callback = {
+        hit : function() { console.log('hit'); }
+    },
+    listening;
 
-require('chai').should();
+
+
 
 describe('Testing Solid Logger - REMOTE', function() {
-    it('should return a valid logger object', function() {
-        logger = Logger.init({
-            adapters: [{
-                type: 'remote',
-                verb: 'GET',
-                application: 'test',
-                machine: 'test-host'
-            }]
+    before(function() {
+        app = express();
+        listening = new BB(function(resolve) {
+            app.get('/log', function(req) {
+                callback.hit(req);
+            });
+            server = app.listen(4321, function() {
+                console.log('server running');
+                resolve();
+            });
         });
-
+    });
+    it('should return a valid logger object', function() {
         logger.should.be.a('object');
     });
 
     it('should send a get request to the configured url', function(done) {
-        var app = express();
 
-        app.get('/log', function(req, res) {
-            console.log('Request: ', req.query);
-            done();
+        listening
+            .then(function() {
+                callback.hit = function() {
+                    done();
+                };
+                logger.debug('critical', 'hi');
+            });
+
+    });
+
+    describe('if category provided', function() {
+
+        it('should send timestamp, application, machine, type, category, and message', function(done) {
+
+            listening
+                .then(function() {
+                    callback.hit = function(req) {
+
+                        should.exist(req.query.timestamp);
+
+                        req.query.application.should.equal('TEST');
+                        req.query.machine.should.equal('TEST-HOST');
+                        req.query.type.should.equal('DEBUG');
+                        req.query.category.should.equal('CRITICAL');
+                        req.query.message.should.equal('hi');
+                        done();
+                    };
+
+                    logger.debug('critical', 'hi');
+                });
         });
+    });
 
-       logger.debug('hi');
+    describe('if category not provided', function() {
 
+        it('should send timestamp, application, machine, type, category, and message', function (done) {
+
+            listening
+                .then(function () {
+
+                    callback.hit = function (req) {
+                        should.exist(req.query.timestamp);
+                        should.not.exist(req.query.category);
+
+                        req.query.application.should.equal('TEST');
+                        req.query.machine.should.equal('TEST-HOST');
+                        req.query.type.should.equal('DEBUG');
+                        req.query.message.should.equal('hi');
+                        done();
+                    };
+
+                    logger.debug('hi');
+                });
+        });
     });
 
 });
